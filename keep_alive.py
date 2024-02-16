d5a9f145a6b251c50c7d634ff424d450232ed016
Beemeo17 <127359555+Beemeo17@users.noreply.github.com>
2024-02-16 10:50:50 +0700

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,23 @@
-from flask import Flask
+import logging
+from flask import Flask, render_template
 from threading import Thread
+import datetime
 
-app = Flask('')
+app = Flask(__name__)
+start_time = datetime.datetime.now()
+
+# Tắt thông báo trên console
+
 
 @app.route('/')
 def home():
-    return "Server is running"
+    current_time = datetime.datetime.now()
+    uptime = current_time - start_time
+    hours = uptime.seconds // 3600
+    return render_template('index.html', uptime=hours)
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
